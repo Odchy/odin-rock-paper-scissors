f3ab9b1d37930427724351d6a44b722ab6b1aa5c
Dennis Bittner <dennisbittner@outlook.com>
2022-09-04 14:06:32 +0200

    Finished game logic and UI

--- external.js
+++ external.js
@@ -1,72 +1,80 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const player = document.querySelector("#playerScore");
+const computer = document.querySelector("#computerScore");
+const roundResult = document.querySelector(".roundResult");
+refreshScore();
+
+function refreshScore(){
+    player.textContent = `Player Score: ${playerScore}`;
+    computer.textContent = `Computer Score: ${computerScore}`;
+}
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * choices.length);
     return choices[random];
 }
 
-function getPlayerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    const input = prompt("Choose: Rock, Paper or Scissors!", "");
-    
-    if(choices.includes(input.toLowerCase()))
-        return input;
-    else {
-        alert("This is not a valid choice!");
-        return getPlayerChoice();
-    }
-}
-
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+function playRound(e) {
+    playerSelection = this.getAttribute("data-key");
+    computerSelection = getComputerChoice();
+    roundResult.classList.remove("rainbow-text");
 
     if (playerSelection == computerSelection)
-        return "It's a tie!";
+        roundResult.textContent = "It's a tie!";
     else if (playerSelection == "rock"){
         if (computerSelection == "paper") {
-            return "You lose! Paper beats Rock!";
+            computerScore++;
+            roundResult.textContent = "You lose! Paper beats Rock!"
         } else {
-            return "You win! Rock beats Scissors!";
+            playerScore++;
+            roundResult.textContent = "You win! Rock beats Scissors!";
         }
     }
     else if (playerSelection == "paper"){
         if (computerSelection == "rock") {
-            return "You win! Paper beats Rock!";
+            playerScore++;
+            roundResult.textContent =  "You win! Paper beats Rock!";
         } else {
-            return "You lose! Scissors beats Paper!";
+            computerScore++;
+            roundResult.textContent =  "You lose! Scissors beats Paper!";
         }
     }
     else if (playerSelection == "scissors"){
         if (computerSelection == "paper") {
-            return "You win! Scissors beats Paper!";
+            playerScore++;
+            roundResult.textContent =  "You win! Scissors beats Paper!";
         } else {
-            return "You lose! Rock beats Scissors!";
+            computerScore++;
+            roundResult.textContent =  "You lose! Rock beats Scissors!";
         }
     }
+
+    refreshScore();
+
+    if (computerScore == 5 || playerScore == 5) 
+        resetGame();
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
+function resetGame(){
 
-        const result = playRound(playerSelection, computerSelection);
-
-        if(result.includes("win"))
-            playerScore++;
-        else if (result.includes("lose"))
-            computerScore++;
-
-        console.log(result);
-    }
     if (playerScore > computerScore) {
-        console.log(`You win! Score: ${playerScore}-${computerScore}`);
-    } else if(playerScore < computerScore) {
-        console.log(`You lose! Score: ${playerScore}-${computerScore}`);
+        roundResult.textContent = "You win! Congratulations! :)";
+        roundResult.classList.add("rainbow-text");
     }
     else
-        console.log(`Draw! Score: ${playerScore}-${computerScore}`);
+        roundResult.textContent = "You lose! Better luck next time :(";
+
+    computerScore = 0;
+    playerScore = 0;
 }
 
-//game();
+
+
+
+
+const choices = document.querySelectorAll('.choice');
+choices.forEach(choice => choice.addEventListener('click', playRound));
+
